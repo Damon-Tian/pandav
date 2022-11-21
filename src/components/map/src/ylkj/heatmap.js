7e877386dae20f6bb8e4c8f60277f3e2d9f523ef
night-white-up <1030884759@qq.com>
2022-11-21 15:02:10 +0800

    1、修改热力图显示层级

--- src/components/map/src/ylkj/heatmap.js
+++ src/components/map/src/ylkj/heatmap.js
@@ -2,7 +2,7 @@
  * @Author: night-white-up 1030884759@qq.com
  * @Date: 2022-11-01 15:35:53
  * @LastEditors: night-white-up 1030884759@qq.com
- * @LastEditTime: 2022-11-01 17:21:40
+ * @LastEditTime: 2022-11-21 14:55:59
  * @FilePath: \pandav\src\components\map\src\ylkj\heatmap.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -32,7 +32,7 @@ export function addHeatMap(map, data) {
             'id': data.id,
             'type': 'heatmap',
             'source': data.id,
-            'maxzoom': 17,
+            // 'maxzoom': 21,
             'paint': {
                 // Increase the heatmap weight based on frequency and property magnitude
                 'heatmap-weight': [
@@ -92,7 +92,7 @@ export function addHeatMap(map, data) {
                     ['zoom'],
                     7,
                     1,
-                    9,
+                    19,
                     0
                 ]
             }
